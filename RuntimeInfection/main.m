2d31b8c478993761ed7fba6b0b9e931028465aff
foreverddong <foreverddong@icloud.com>
2015-06-29 11:18:01 +0800

    Initial Commit

---
+++ RuntimeInfection/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeInfection
+//
+//  Created by Brian Hsu on 15/6/29.
+//  Copyright (c) 2015年 Brian Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
